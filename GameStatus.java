b8f2dfc70511355a18313bf8949a5dcdea08733a
etudie <39594459+etudie@users.noreply.github.com>
2019-02-28 21:01:03 -0500

    Update GameStatus.java

--- GameStatus.java
+++ GameStatus.java
@@ -1,6 +1,18 @@
+/*****************************************************************
+ Handles GameStatus of mineSweeper.
+
+ @author Xue Hua
+ @version Winter 2019
+ *****************************************************************/
 package MineSweeper;
 
-
+/*****************************************************************
+ Returns GameStatus.
+ @param void
+ @return Lost       Game is lost
+         Won        Game is won
+        NotOverYet  Game is not won and not lost
+ *****************************************************************/
 public enum GameStatus {
     Lost, Won, NotOverYet
 }
